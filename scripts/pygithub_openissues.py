c808a33d22aaec84aed05e7abad15e1324313e3f
surendba <surendba@cba.com.au>
2024-06-24 21:27:55 +1000

    fixes-1

--- scripts/pygithub_openissues.py
+++ scripts/pygithub_openissues.py
@@ -3,12 +3,16 @@ import os
 
 token = os.getenv('GITHUB_TOKEN')
 repo = os.getenv('GITHUB_REPOSITORY')
+owner = os.getenv('GITHUB_REPOSITORY_OWNER')
 
-repository_full_name = f"surendba/{repo}"
+print(f"Repo is  : {repo}")
+print(f"Owner is : {owner}")
+
+repository_full_name = f"/repos/{owner}/{repo}"
 
 g = Github(token)
 
-repo = g.get_repo(repository_full_name)
+repo = g.get_repo()
 
 open_issues = repo.get_issues(state='open')
 
