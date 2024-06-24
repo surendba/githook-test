7ec9d5f1e0bcd2eca40e8d16b39c9b9b1d804723
surendba <surendba@cba.com.au>
2024-06-24 21:58:35 +1000

    fixes-6

--- scripts/pygithub_openissues.py
+++ scripts/pygithub_openissues.py
@@ -16,6 +16,9 @@ open_issues = repo.get_issues(state='open')
 
 for issue in open_issues:
     print(f"Issue #{issue.number}: {issue.title}")
+    #make a comment on the issue
+    issue.comments = "Closing this issue from github action..."
+    issue.edit(state='closed')
 
 
 
