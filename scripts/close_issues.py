649797a5e76d9ded715291d13c1b3ccf75e96448
surendba <surendba@cba.com.au>
2024-06-24 19:23:40 +1000

    fixed typo

--- scripts/close_issues.py
+++ scripts/close_issues.py
@@ -22,7 +22,7 @@ def close_issues():
             issue_number = issue["number"]
             close_url = f"{url}/{issue_number}"
             data = {"state": "closed"}
-            close_respose = requests.patch(close_url, headers=headers, json=data=)
+            close_respose = requests.patch(close_url, headers=headers, json=data)
             if close_respose.status_code == 200:
                 print(f"Issue #{issue_number} is closed.")
             else
